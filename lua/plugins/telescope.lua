4440a9de2d41ecd5aa5e74edcdab409063a83523
harryhjsh <39673043+harryhjsh@users.noreply.github.com>
2025-08-01 00:19:08 +0100

    fzf build

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,25 +1,22 @@
 return {
-	"nvim-telescope/telescope.nvim",
-	event = "VimEnter",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		{
-			"nvim-telescope/telescope-fzf-native.nvim",
-			build = "make",
-			cond = function()
-				return vim.fn.executable 'make' == 1
-			end,
-		},
-		{ 'nvim-telescope/telescope-ui-select.nvim' },
-		{ 'nvim-tree/nvim-web-devicons' },
-	},
-	keys = {
-		{
-			'<leader><leader>',
-			"<cmd>Telescope find_files<cr>",
-			mode = "n",
-			desc = "Find files"
-		},
-
-	},
+  "nvim-telescope/telescope.nvim",
+  event = "VimEnter",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    {
+      "nvim-telescope/telescope-fzf-native.nvim",
+      -- TODO windows-only
+      build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release",
+    },
+    { "nvim-telescope/telescope-ui-select.nvim" },
+    { "nvim-tree/nvim-web-devicons" },
+  },
+  keys = {
+    {
+      "<leader><leader>",
+      "<cmd>Telescope find_files<cr>",
+      mode = "n",
+      desc = "Find files",
+    },
+  },
 }
