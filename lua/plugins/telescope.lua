2c5ae324def4dae8d44c4356c973ca3a1e212c23
harryhjsh <39673043+harryhjsh@users.noreply.github.com>
2025-08-01 13:52:38 +0100

    add local autoscope plugin + binds

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,22 +1,38 @@
 return {
-  "nvim-telescope/telescope.nvim",
-  event = "VimEnter",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    {
-      "nvim-telescope/telescope-fzf-native.nvim",
-      -- TODO windows-only
-      build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release",
+  {
+    "nvim-telescope/telescope.nvim",
+    event = "VimEnter",
+    config = function()
+      require("telescope").setup({})
+    end,
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      {
+        "nvim-telescope/telescope-fzf-native.nvim",
+        build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release",
+      },
+      { "nvim-telescope/telescope-ui-select.nvim" },
+      { "nvim-tree/nvim-web-devicons" },
+      {
+        dir = "C:/dev/github/telescope-autoscope",
+        name = "telescope-autoscope.nvim",
+        config = function()
+          require("autoscope").setup()
+        end,
+      },
     },
-    { "nvim-telescope/telescope-ui-select.nvim" },
-    { "nvim-tree/nvim-web-devicons" },
-  },
-  keys = {
-    {
-      "<leader><leader>",
-      "<cmd>Telescope find_files<cr>",
-      mode = "n",
-      desc = "Find files",
+    keys = {
+      {
+        "<leader>ff",
+        "<cmd>Telescope find_files<cr>",
+        mode = "n",
+        desc = "[f]ind",
+      },
+      {
+        "<leader><leader>",
+        "<cmd>Telescope autoscope find_files<cr>",
+        desc = "find files in package",
+      },
     },
   },
 }
